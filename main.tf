0a8bdea09def7cdb5a5c9d1b23af9162abb47b88
Durga2devops <163587440+Durga2devops@users.noreply.github.com>
2026-02-06 10:30:30 +0530

    Feature/db 123 ec2 fix (#1)
    * Add Terraform Day1 EC2 configuration
    
    * Added Ec2 for dev
    
    * DB-123-Deleted Day1

--- main.tf
+++ main.tf
@@ -2,11 +2,54 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Dynamically fetch latest Amazon Linux 2
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["110.235.236.78/32"] # Never use 0.0.0.0/0 in enterprise
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "example" {
-  ami           = "ami-0532be01f26a3de55" # Amazon Linux 2 AMI
+
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  root_block_device {
+    encrypted = true
+  }
+
   tags = {
-    Name = "MyTerraformInstance"
+    Name        = "Dev-Terraform-Instance"
+    Environment = "Dev"
+    Owner       = "DevOps"
+    Terraform   = "true"
   }
 }
